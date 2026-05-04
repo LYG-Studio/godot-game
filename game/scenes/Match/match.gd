422cb80725405bddc94fb40fdf7810541b93fc96
LidiaYoshino <lidiayoshino@gmail.com>
2026-05-04 19:38:54 -0300

    feat: WIP Match script - adding consts and vars

--- game/scenes/Match/match.gd
+++ game/scenes/Match/match.gd
@@ -1,13 +1,28 @@
 extends Node2D
 
 # receives the deck the match will use
+const DECK = [
+	{"type": "Copas", "value": 1}, # should we add the ID and card image/animations?
+	{"type": "Copas", "value": 2},
+	{"type": "Copas", "value": 3},
+	{"type": "Copas", "value": 4},
+	{"type": "Copas", "value": 5},
+	{"type": "Copas", "value": 6},
+	{"type": "Copas", "value": 7},
+] # leaving it hardcoded for now to get the match rules done
+
 # max health could be received as well if we plan changing it from match to match
+const MAX_HEALTH = 20 # leaving it hardcoded for now
+const ROOM_SIZE = 4
+
+var currentHealth = MAX_HEALTH
 
 # controls all the general rules of a match:
 # - 4 cards make a room
-# - users picks up to 3 and then new cards are added unil there are 4 again
-# - picked cards are activated and resolved if there is need for user input and then the card is discarted
-# - only one potion can be used with the healing effect per room, subsequent ones need to be discarted
+# - users picks up to 3 and then new cards are added unil there are 4 again or until the deck runs
+#      out of cards
+# - picked cards are activated and resolved if there is need for user input and then the card is discarded
+# - only one potion can be used with the healing effect per room, subsequent ones need to be discarded
 # - the weapon can only be used against enemies of lower power than the last defeated one.
 #      If no enemy was defeated then there is no limit.
 # - if a weapon can not be used, the fight will be bare handed, receiving full damage
